9711c85ec59f276c7d7d21e22de0c3118e26fda8
shaysadin <shay.sadina29@gmail.com>
2023-08-08 15:06:34 +0300

    Uploading project

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import Home from './pages/Home'
+import Canvas from './canvas'
+import Custominzer from './pages/Custominzer'
+
+
+function App() {
+
+
+  return (
+    <main className="app transition-all ease-in">
+    <Home />
+    <Canvas />
+    <Custominzer />
+    </main>
+  )
+}
+
+export default App
